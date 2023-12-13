123125e74296ccd103795669f316f6f488d980b0
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-12-13 20:47:18 +0700

    feat(enum): add enum data type

---
+++ enums/EPrintType.ts
@@ -0,0 +1,22 @@
+/**
+ * Coding service by Sleepy4k <sarahpalastring@gmail.com>
+ *
+ * Reselling this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by:
+ * Apri Pandu Wicaksono
+ *
+ * Link: https://github.com/sleepy4k
+ *
+ * March 12, 2023
+ */
+enum EPrintType {
+  ERROR = "error",
+  WARN = "warn",
+  DEBUG = "debug",
+  INFO = "info",
+  DEFAULT = "default"
+}
+
+export default EPrintType;
