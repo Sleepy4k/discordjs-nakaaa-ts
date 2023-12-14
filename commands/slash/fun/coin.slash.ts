e2fb10a808b267bbda30291216980644e32486fa
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-12-14 17:36:29 +0700

    feat(command): add basic slash command

---
+++ commands/slash/fun/coin.slash.ts
@@ -0,0 +1,80 @@
+/**
+ * Coding service by Sleepy4k <sarahpalastring@gmail.com>
+ *
+ * Reselling this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by:
+ * Apri Pandu Wicaksono
+ *
+ * Link: https://github.com/sleepy4k
+ *
+ * March 12, 2023
+ */
+import { Bot } from "@server/bot";
+import { ESlashOpt } from "@enums";
+import { Command } from "@templates";
+import type { TMessageFunc } from "@types";
+import main from "@functions/fun/coin.func";
+import { Message, PermissionFlagsBits, ApplicationCommandType, ChatInputCommandInteraction } from "discord.js";
+
+export default new Command({
+  /**
+   * Event name
+   * @type {string}
+   */
+  name: "coin",
+
+  /**
+   * Event description
+   * @type {string}
+   */
+  description: "Flip a coin.",
+
+  /**
+   * Event user permissions
+   * @type {bigint}
+   */
+  userPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event bot permissions
+   * @type {bigint}
+   */
+  botPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event category
+   * @type {string}
+   */
+  category: "fun",
+
+  /**
+   * Event type
+   * @type {ApplicationCommandType}
+   */
+  type: ApplicationCommandType.ChatInput,
+
+  /**
+   * Event options
+   * @type {Array<object>}
+   */
+  options: [{
+    name: "guess",
+    description: "Your guess",
+    type: ESlashOpt.STRING,
+    choices: [
+      { name: "Heads", value: "heads" },
+      { name: "Tails", value: "tails" }
+    ],
+    required: true
+  }],
+
+  /**
+   * Event function
+   * @type {TMessageFunc}
+   */
+  run: async (client: Bot, interaction: Message | ChatInputCommandInteraction, args: any[], prefix: string) => {
+    await main({ client, interaction, args, prefix });
+  }
+});
