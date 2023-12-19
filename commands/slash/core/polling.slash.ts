b4ec76018622d0486474cbe5268258c208591561
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-12-19 09:33:46 +0700

    feat(command): add core command for slash and message

---
+++ commands/slash/core/polling.slash.ts
@@ -0,0 +1,75 @@
+/**
+ * Coding service by Sleepy4k <sarahpalastring@gmail.com>
+ *
+ * Reselling this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by:
+ * Apri Pandu Wicaksono
+ *
+ * Link: https://github.com/sleepy4k
+ *
+ * March 12, 2023
+ */
+import { Bot } from "@server/bot";
+import { ESlashOpt } from "@enums";
+import { Command } from "@templates";
+import main from "@functions/core/polling.func";
+import { Message, PermissionFlagsBits, ApplicationCommandType, ChatInputCommandInteraction } from "discord.js";
+
+export default new Command({
+  /**
+   * Event name
+   * @type {string}
+   */
+  name: "polling",
+
+  /**
+   * Event description
+   * @type {string}
+   */
+  description: "Make a polling.",
+
+  /**
+   * Event user permissions
+   * @type {bigint}
+   */
+  userPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event bot permissions
+   * @type {bigint}
+   */
+  botPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event category
+   * @type {string}
+   */
+  category: "core",
+
+  /**
+   * Event type
+   * @type {ApplicationCommandType}
+   */
+  type: ApplicationCommandType.ChatInput,
+
+  /**
+   * Event options
+   * @type {Array<object>}
+   */
+  options: [{
+    name: "polling",
+    description: "Polling question.",
+    type: ESlashOpt.STRING,
+    required: true
+  }],
+
+  /**
+   * Event function
+   * @type {TCommandFunc}
+   */
+  run: async (client: Bot, interaction: Message | ChatInputCommandInteraction, args: any[], prefix: string) => {
+    await main({ client, interaction, args, prefix });
+  }
+});
