06f62f3334c0ad8ea4c246a4285b3eca0debafaa
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-12-19 18:33:09 +0700

    feat(command): add misc command for slash and message

---
+++ commands/message/misc/uptime.message.ts
@@ -0,0 +1,63 @@
+/**
+ * Coding service by Sleepy4k <sarahpalastring@gmail.com>
+ *
+ * Reselling this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by:
+ * Apri Pandu Wicaksono
+ *
+ * Link: https://github.com/sleepy4k
+ *
+ * March 12, 2023
+ */
+import { Bot } from "@server/bot";
+import { Command } from "@templates";
+import main from "@functions/misc/uptime.func";
+import { Message, PermissionFlagsBits, ChatInputCommandInteraction } from "discord.js";
+
+export default new Command({
+  /**
+   * Event name
+   * @type {string}
+   */
+  name: "uptime",
+
+  /**
+   * Event description
+   * @type {string}
+   */
+  description: "Show bot uptime.",
+
+  /**
+   * Event user permissions
+   * @type {bigint}
+   */
+  userPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event bot permissions
+   * @type {bigint}
+   */
+  botPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event category
+   * @type {string}
+   */
+  category: "misc",
+
+  /**
+   * Event cooldown
+   * @type {number}
+   */
+  cooldown: 5,
+
+  /**
+   * Event function
+   * @type {TCommandFunc}
+   */
+  run: async (client: Bot, interaction: Message | ChatInputCommandInteraction, args: any[], prefix: string) => {
+    await main({ client, interaction, args, prefix });
+  }
+});
---
+++ commands/slash/misc/ping.slash.ts
@@ -0,0 +1,69 @@
+/**
+ * Coding service by Sleepy4k <sarahpalastring@gmail.com>
+ *
+ * Reselling this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by:
+ * Apri Pandu Wicaksono
+ *
+ * Link: https://github.com/sleepy4k
+ *
+ * March 12, 2023
+ */
+import { Bot } from "@server/bot";
+import { Command } from "@templates";
+import main from "@functions/misc/ping.func";
+import { Message, PermissionFlagsBits, ApplicationCommandType, ChatInputCommandInteraction } from "discord.js";
+
+export default new Command({
+  /**
+   * Event name
+   * @type {string}
+   */
+  name: "ping",
+
+  /**
+   * Event description
+   * @type {string}
+   */
+  description: "Show your ping to the bot.",
+
+  /**
+   * Event user permissions
+   * @type {bigint}
+   */
+  userPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event bot permissions
+   * @type {bigint}
+   */
+  botPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event category
+   * @type {string}
+   */
+  category: "misc",
+
+  /**
+   * Event type
+   * @type {ApplicationCommandType}
+   */
+  type: ApplicationCommandType.ChatInput,
+
+  /**
+   * Event options
+   * @type {Array<object>}
+   */
+  options: [],
+
+  /**
+   * Event function
+   * @type {TCommandFunc}
+   */
+  run: async (client: Bot, interaction: Message | ChatInputCommandInteraction, args: any[], prefix: string) => {
+    await main({ client, interaction, args, prefix });
+  }
+});
