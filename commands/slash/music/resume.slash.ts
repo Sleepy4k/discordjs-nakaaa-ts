7bf61b362f776c9dbf8687830394af509228e5ae
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-12-26 22:37:06 +0700

    feat(command): add music feature for message and slash

---
+++ commands/slash/music/resume.slash.ts
@@ -0,0 +1,69 @@
+/**
+ * Coding service by Sleepy4k <sarahpalastring@gmail.com>
+ *
+ * Reselling this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by:
+ * Apri Pandu Wicaksono
+ *
+ * Link: https://github.com/sleepy4k
+ *
+ * March 12, 2023
+ */
+import { Bot } from "@server/bot";
+import { Command } from "@templates";
+import main from "@functions/music/resume.func";
+import { Message, PermissionFlagsBits, ApplicationCommandType, ChatInputCommandInteraction } from "discord.js";
+
+export default new Command({
+  /**
+   * Event name
+   * @type {string}
+   */
+  name: "resume",
+
+  /**
+   * Event description
+   * @type {string}
+   */
+  description: "Resume the current song.",
+
+  /**
+   * Event user permissions
+   * @type {bigint}
+   */
+  userPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event bot permissions
+   * @type {bigint}
+   */
+  botPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event category
+   * @type {string}
+   */
+  category: "music",
+
+  /**
+   * Event type
+   * @type {ApplicationCommandType}
+   */
+  type: ApplicationCommandType.ChatInput,
+
+  /**
+   * Event options
+   * @type {Array<object>}
+   */
+  options: [],
+
+  /**
+   * Event function
+   * @type {TCommandFunc}
+   */
+  run: async (client: Bot, interaction: Message | ChatInputCommandInteraction, args: any[], prefix: string) => {
+    await main({ client, interaction, args, prefix });
+  }
+});
---
+++ commands/slash/music/skip.slash.ts
@@ -0,0 +1,69 @@
+/**
+ * Coding service by Sleepy4k <sarahpalastring@gmail.com>
+ *
+ * Reselling this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by:
+ * Apri Pandu Wicaksono
+ *
+ * Link: https://github.com/sleepy4k
+ *
+ * March 12, 2023
+ */
+import { Bot } from "@server/bot";
+import { Command } from "@templates";
+import main from "@functions/music/skip.func";
+import { Message, PermissionFlagsBits, ApplicationCommandType, ChatInputCommandInteraction } from "discord.js";
+
+export default new Command({
+  /**
+   * Event name
+   * @type {string}
+   */
+  name: "skip",
+
+  /**
+   * Event description
+   * @type {string}
+   */
+  description: "Skip the current song.",
+
+  /**
+   * Event user permissions
+   * @type {bigint}
+   */
+  userPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event bot permissions
+   * @type {bigint}
+   */
+  botPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event category
+   * @type {string}
+   */
+  category: "music",
+
+  /**
+   * Event type
+   * @type {ApplicationCommandType}
+   */
+  type: ApplicationCommandType.ChatInput,
+
+  /**
+   * Event options
+   * @type {Array<object>}
+   */
+  options: [],
+
+  /**
+   * Event function
+   * @type {TCommandFunc}
+   */
+  run: async (client: Bot, interaction: Message | ChatInputCommandInteraction, args: any[], prefix: string) => {
+    await main({ client, interaction, args, prefix });
+  }
+});
