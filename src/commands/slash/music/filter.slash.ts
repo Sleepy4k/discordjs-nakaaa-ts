50e10ad7bbf24cf22d9651cd3f669a8c2b4039d7
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-12-28 09:02:56 +0700

    feat(command): add music filter command for slash and message

---
+++ src/commands/slash/music/filter.slash.ts
@@ -0,0 +1,80 @@
+/**
+ * Coding service by Sleepy4k <sarahpalastring@gmail.com>
+ *
+ * Reselling this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by:
+ * Apri Pandu Wicaksono
+ *
+ * Link: https://github.com/sleepy4k
+ *
+ * March 12, 2023
+ */
+import config from "@config";
+import { Bot } from "@server/bot";
+import { ESlashOpt } from "@enums";
+import { Command } from "@templates";
+import main from "@functions/music/filter.func";
+import { Message, PermissionFlagsBits, ApplicationCommandType, ChatInputCommandInteraction } from "discord.js";
+
+export default new Command({
+  /**
+   * Event name
+   * @type {string}
+   */
+  name: "filter",
+
+  /**
+   * Event description
+   * @type {string}
+   */
+  description: "Set the filter of the music.",
+
+  /**
+   * Event user permissions
+   * @type {bigint}
+   */
+  userPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event bot permissions
+   * @type {bigint}
+   */
+  botPermissions: PermissionFlagsBits.SendMessages,
+
+  /**
+   * Event category
+   * @type {string}
+   */
+  category: "music",
+
+  /**
+   * Event type
+   * @type {ApplicationCommandType}
+   */
+  type: ApplicationCommandType.ChatInput,
+
+  /**
+   * Event options
+   * @type {Array<object>}
+   */
+  options: [{
+    name: "filter",
+    description: "The filter of the music.",
+    type: ESlashOpt.STRING,
+    choices: config.filter.music.map((filter) => ({
+      name: filter,
+      value: (filter != "8D" ? filter.toLowerCase() : "8D")
+    })),
+    required: true
+  }],
+
+  /**
+   * Event function
+   * @type {TCommandFunc}
+   */
+  run: async (client: Bot, interaction: Message | ChatInputCommandInteraction, args: any[], prefix: string) => {
+    await main({ client, interaction, args, prefix });
+  }
+});
