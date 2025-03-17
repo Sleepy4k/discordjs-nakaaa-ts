742c47651d8c9022527c6912916bc9e9c04624e8
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-03-18 02:24:57 +0700

    feat(enum): add OS, Slash, and update Print type enum

---
+++ src/enums/EOSType.ts
@@ -0,0 +1,16 @@
+enum EOSType {
+  aix = "IBM AIX",
+  linux = "Linux",
+  sunos = "SunOS",
+  darwin = "Darwin",
+  win32 = "Windows",
+  openbsd = "OpenBSD",
+  freebsd = "FreeBSD",
+  android = "Android",
+  cygwin = "Cygwin",
+  netbsd = "NetBSD",
+  haiku = "Haiku",
+  os390 = "IBM OS/390"
+}
+
+export default EOSType;
--- src/enums/EPrintType.ts
+++ src/enums/EPrintType.ts
@@ -1,10 +1,10 @@
 enum EPrintType {
-  ERROR = "error",
-  WARN = "warn",
-  DEBUG = "debug",
-  INFO = "info",
-  WEB = "web",
-  DEFAULT = "default"
+  ERROR = "ERROR",
+  WARN = "WARN",
+  DEBUG = "DEBUG",
+  INFO = "INFO",
+  WEB = "WEB",
+  DEFAULT = "DEFAULT"
 };
 
 export default EPrintType;
