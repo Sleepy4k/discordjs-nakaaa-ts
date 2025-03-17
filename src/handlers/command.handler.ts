881c5bfad1006888aa0f9ae7314ee5644712b783
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-03-18 02:28:07 +0700

    feat(handler): add command handler and add error handler on event

---
+++ src/handlers/command.handler.ts
@@ -0,0 +1,72 @@
+import ELogStatus from "@enums/ELogStatus.js";
+import Handler from "@templates/Handler.js";
+import CatchError from "@classes/CatchError.js";
+import { readdir } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { ICommandFile } from "@interfaces/commandFile.js";
+import TBotClient from "@interfaces/botClient.js";
+
+const __dirname = path.resolve(fileURLToPath(import.meta.url) + "/../../");
+const __filename = path.basename(fileURLToPath(import.meta.url));
+
+export default new Handler({
+  name: "command",
+
+  run: async (client: TBotClient): Promise<void> => {
+    try {
+      const loadMessageFile = async (filePath: string, fileName: string) => {
+        client.logStatus(fileName, "Slash", ELogStatus.LOADING);
+        client.logStatus(fileName, "Message", ELogStatus.LOADING);
+        try {
+          const command: ICommandFile = await import(filePath).then((module) => module.default);
+          if (!command.name) {
+            client.logStatus(fileName, "Message", ELogStatus.ERROR);
+            return;
+          }
+
+          if (command.enableSlash) {
+            client.slashCommands.set(command.name, command);
+            client.logStatus(fileName, "Slash", ELogStatus.SUCCESS);
+          }
+
+          client.messageCommands.set(command.name, command);
+          client.logStatus(`${fileName} (${command.name})`, "Message", ELogStatus.SUCCESS);
+
+          if (!command.aliases) return;
+
+          const aliases = Array.isArray(command.aliases) ? command.aliases : [command.aliases];
+          aliases.forEach((alias) => {
+            client.logStatus(`${alias} (${command.name})`, "Message", ELogStatus.LOADING);
+            client.messageCommands.set(alias, command);
+            client.logStatus(`${alias} (${command.name})`, "Message", ELogStatus.SUCCESS);
+          });
+        } catch {
+          client.logStatus(fileName, "Slash", ELogStatus.ERROR);
+          client.logStatus(fileName, "Message", ELogStatus.ERROR);
+        }
+      };
+
+      const messages = await readdir(`${__dirname}/commands`);
+      const extension = __filename.split(".").pop();
+
+      const filteredMessages = messages.filter((file) => file.endsWith(`.${extension}`));
+      await Promise.all(filteredMessages.map((file) => {
+        const fileName = file.split(".")[0];
+        return loadMessageFile(`../commands/${file}`, fileName);
+      }));
+
+      const subDirectories = messages.filter((file) => !file.endsWith(`.${extension}`));
+      for (const subDirectory of subDirectories) {
+        const subFiles = await readdir(`${__dirname}/commands/${subDirectory}`);
+        const filteredSubFiles = subFiles.filter((file) => file.endsWith(`.${extension}`));
+        await Promise.all(filteredSubFiles.map((file) => {
+          const fileName = file.split(".")[0];
+          return loadMessageFile(`../commands/${subDirectory}/${file}`, fileName);
+        }));
+      }
+    } catch (error) {
+      CatchError.print(error);
+    }
+  }
+});
--- src/handlers/event.handler.ts
+++ src/handlers/event.handler.ts
@@ -1,11 +1,11 @@
 import ELogStatus from "@enums/ELogStatus.js";
 import CatchError from "@classes/CatchError.js";
-import Bot from "@modules/bot.js";
-import Handler from "@templates/handler.js";
+import Handler from "@templates/Handler.js";
 import { readdir } from "node:fs/promises";
 import type { IEventFile } from "@interfaces/eventFile.js";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import TBotClient from "@interfaces/botClient.js";
 
 const __dirname = path.resolve(fileURLToPath(import.meta.url) + "/../../");
 const __filename = path.basename(fileURLToPath(import.meta.url));
@@ -13,29 +13,30 @@ const __filename = path.basename(fileURLToPath(import.meta.url));
 export default new Handler({
   name: "event",
 
-  run: async (client: Bot): Promise<void> => {
+  run: async (client: TBotClient): Promise<void> => {
     try {
       const events = await readdir(`${__dirname}/events`);
-      const extension = __filename.split(".").pop();
-      const filteredEvents = events.filter((file) => file.endsWith(`.${extension}`));
+      const extension = path.extname(__filename);
+      const filteredEvents = events.filter((file) => file.endsWith(extension));
 
       await Promise.all(filteredEvents.map(async (filteredEvent) => {
-        const fileName = filteredEvent.split(".");
-        client.logStatus(fileName[0], "Event", ELogStatus.LOADING);
-        const event: IEventFile = await import(`../events/${fileName[0]}.${fileName[1]}`)
-          .then((event) => event.default)
-          .catch((_err) => {
-            return { name: null };
-          });
+        const [fileName] = filteredEvent.split(".");
+        client.logStatus(fileName, "Event", ELogStatus.LOADING);
+        try {
+          const event: IEventFile = (await import(`../events/${filteredEvent}`)).default;
 
-        if (!event.name) client.logStatus(fileName[0], "Event", ELogStatus.ERROR);
-        else {
-          client.events.set(event.name, event);
-          client.on(event.name, (...args) => event.run(client, ...args));
-          client.logStatus(`${fileName[0]} (${event.name})`, "Event", ELogStatus.SUCCESS);
+          if (!event.name) {
+            client.logStatus(fileName, "Event", ELogStatus.ERROR);
+          } else {
+            client.events.set(event.name, event);
+            client.on(event.name, (...args) => event.run(client, ...args));
+            client.logStatus(`${fileName} (${event.name})`, "Event", ELogStatus.SUCCESS);
+          }
+        } catch {
+          client.logStatus(fileName, "Event", ELogStatus.ERROR);
         }
       }));
-    } catch (error: unknown) {
+    } catch (error) {
       CatchError.print(error);
     }
   }
