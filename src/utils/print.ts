4f76d1cd6c8ac9630cce716be8cac3754605adae
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-03-18 02:26:08 +0700

    chore(util): clean up and add parse method

--- src/utils/print.ts
+++ src/utils/print.ts
@@ -2,38 +2,22 @@ import LogToFile from "@classes/LogToFile.js";
 import EPrintType from "@enums/EPrintType.js";
 import { getCurrentDateWithTime } from "./parse.js";
 
+/**
+ * Print message to console and log to file
+ * @param {EPrintType} type
+ * @param {string} message
+ * @returns {void}
+ * @example
+ * ```
+ * print(EPrintType.INFO, "Hello, world!");
+ * ```
+ */
 const print = (type: EPrintType, message: string): void => {
-  let data;
-
   const currDate = getCurrentDateWithTime();
+  const typeString = EPrintType[type] || EPrintType.DEFAULT;
+  const data = `[${currDate}] [${typeString}] ${message}`;
 
-  switch (type) {
-  case EPrintType.INFO:
-    data = `[${currDate}] [INFO] ${message}`;
-    LogToFile.write(EPrintType.INFO, data);
-    break;
-  case EPrintType.WARN:
-    data = `[${currDate}] [WARN] ${message}`;
-    LogToFile.write(EPrintType.WARN, data);
-    break;
-  case EPrintType.DEBUG:
-    data = `[${currDate}] [DEBUG] ${message}`;
-    LogToFile.write(EPrintType.DEBUG, data);
-    break;
-  case EPrintType.ERROR:
-    data = `[${currDate}] [ERROR] ${message}`;
-    LogToFile.write(EPrintType.ERROR, data);
-    break;
-  case EPrintType.WEB:
-    data = `[${currDate}] [WEB] ${message}`;
-    LogToFile.write(EPrintType.WEB, data);
-    break;
-  default:
-    data = `[${currDate}] ${message}`;
-    LogToFile.write(EPrintType.DEFAULT, data);
-    break;
-  }
-
+  LogToFile.write(type, data);
   console.log(data);
 };
 
